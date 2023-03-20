379e9c87f90c3d457ab91c7fff5e4bf836d71388
AlfredRosenthal <alfred.rosenthal@quartech.com>
2023-03-20 16:56:21 -0700

    added other species upsert/ configs for summary results

--- utils/templates/summary/sql/Elk_Summary_Results_UPSERT.sql
+++ utils/templates/summary/sql/Elk_Summary_Results_UPSERT.sql
@@ -4,14 +4,14 @@ set schema 'biohub';
 do $$
 	declare
 		_summary_template_id integer; -- set after template is created
-		_template_name varchar := 'Template Name 1';
+		_template_name varchar := 'Elk Summary Results';
 		_template_version varchar := '2.0';
 		_template_description varchar := '';
 		
-		_taxonomy_ids integer[] := array[1,2,3]; -- wild taxonomic IDs from elastic search
+		_taxonomy_ids integer[] := array[35370, 35371]; -- wild taxonomic IDs from elastic search
 		_taxonomy_id integer; -- used as an index in the loop, do not set
 
-		_validation_schema varchar := $v_s${}$v_s$;
+		_validation_schema varchar := $v_s${"name":"","description":"","files":[{"name":"Elk_RESULTS","description":"","validations":[{"file_duplicate_columns_validator":{}},{"file_required_columns_validator":{"required_columns":["Study Area","Population Unit","Block ID/SU ID","Survey Start Date","Survey End Date","Survey Year","Survey Month","Survey Day","Total Survey Time","Total Survey Time Unit Code","Total Area Surveyed (km2)","Total Kilometers Surveyed (km)","Species Code","Parameter","Stratum","Observed","Estimated","Sightability Model","Sightability Correction Factor","SE","Coefficient of Variation (%)","Confidence Level (%)","Lower CL","Upper CL","Best Parameter Value Flag","Outlier SRB Blocks Removed","Marked Animals Observed","Total Marked Animals Available","Comments"]}}],"columns":[]}],"validations":[{"mimetype_validator":{"reg_exps":["text/csv","application/vnd.*"]}}]}$v_s$;
 begin
   INSERT INTO
     summary_template (name, version, record_effective_date, description)
